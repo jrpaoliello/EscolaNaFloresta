06c268b8411f3e9b3ee5adbc6a60c8b9ca52ef0e
jose paoliello <jrpaoliello@gmail.com>
2019-09-19 09:00:53 -0300

    primeiros arquivos cs

---
+++ Assets/Scripts/Beans/Exercicio.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercicio
+{
+    private int numeroExercicio;
+    private Dictionary<int, AnimationClip> animacoes;
+}
---
+++ Assets/Scripts/Beans/Fase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fase
+{
+    private int numeroFase;
+    private Dictionary<int,Exercicio> exercicios;
+
+}
